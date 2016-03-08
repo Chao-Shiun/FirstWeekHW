e65f8767a674d3cc99c53f90a07744d7869468f1
Chao-Shiun <e4366.tw@gmail.com>
2016-03-08 22:44:55 +0800

    資料庫新增已刪除欄位

--- ClientManagementHomework/Models/客戶資料.cs
+++ ClientManagementHomework/Models/客戶資料.cs
@@ -11,7 +11,7 @@ namespace ClientManagementHomework.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
+    
     public partial class 客戶資料
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,25 +20,16 @@ namespace ClientManagementHomework.Models
             this.客戶銀行資訊 = new HashSet<客戶銀行資訊>();
             this.客戶聯絡人 = new HashSet<客戶聯絡人>();
         }
-
+    
         public int Id { get; set; }
-        [Required]
-        [StringLength(50, ErrorMessage = "客戶名稱不可超過50個字")]
         public string 客戶名稱 { get; set; }
-        [Required]
-        [StringLength(8, ErrorMessage = "統一編號不可超過8個字")]
         public string 統一編號 { get; set; }
-        [Required]
-        [StringLength(50, ErrorMessage = "電話號碼不可超過50個字")]
         public string 電話 { get; set; }
-        [StringLength(50, ErrorMessage = "傳真號碼不可超過50個字")]
         public string 傳真 { get; set; }
-        [StringLength(100, ErrorMessage = "地址不可超過100個字")]
         public string 地址 { get; set; }
-        [StringLength(250, ErrorMessage = "Email不可超過250個字")]
-        [EmailAddress(ErrorMessage = "這不是一個有效的Email")]
         public string Email { get; set; }
-
+        public bool 已刪除 { get; set; }
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
